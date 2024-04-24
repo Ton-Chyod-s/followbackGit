d71786a94f70bdd2e5758caaa515196a58943b9e
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-24 14:24:06 -0400

    doc: att

--- src/git.js
+++ src/git.js
@@ -2,19 +2,12 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 async function main(username) {
-    const siteSeguindo = 'https://github.com/Ton-Chyod-s?tab=followers'
-    const siteSeguidores = 'https://github.com/Ton-Chyod-s?tab=following'
-
     const perfil = await fetch(`https://api.github.com/users/${username}`);
     const data = await perfil.json();
     const seguidores = `${data.following}`
 
-    const responseSeguindo = await axios.get(siteSeguindo);
-    const $Seguindo = cheerio.load(responseSeguindo.data);
-    const cards = $Seguindo(".js-profile-following");
-
-    const responseSeguidores = await axios.get(siteSeguidores);
-    const $Seguidores = cheerio.load(responseSeguidores.data);
+   
+    
   }
   
   if (require.main === module) {
