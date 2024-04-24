e6d61e05ae5c26a2ae5f17be6f4ba7b11113dc25
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-24 17:17:09 -0400

    doc: att

--- src/git.js
+++ src/git.js
@@ -1,36 +1,30 @@
 async function main(username) {
     const seguidores = [];
-    const followers = await fetch(`https://api.github.com/users/${username}/followers`);
-    const dataFollowers = await followers.json();
+    const followersResponse = await fetch(`https://api.github.com/users/${username}/followers`);
+    const dataFollowers = await followersResponse.json();
     for (let i = 0; i < dataFollowers.length; i++) {
         seguidores.push(dataFollowers[i].login);
     }
 
     const seguindo = [];
-    const following = await fetch(`https://api.github.com/users/${username}/following`);
-    const dataFollowing = await following.json();
+    const followingResponse = await fetch(`https://api.github.com/users/${username}/following`);
+    const dataFollowing = await followingResponse.json();
     for (let i = 0; i < dataFollowing.length; i++) {
         seguindo.push(dataFollowing[i].login);
     }
+
     const seguidorMutuo = {};
     const naoSeguidor = {};
-    let cont = 0;
-    
+
     for (let i = 0; i < seguindo.length; i++) {
-        for (let j = 0; j < seguidores.length; j++) {
-            const segdores = seguidores[i];
-            const segdo = seguindo[j];
-            if (segdo === segdores) {
-                cont += 1;
-                seguidorMutuo[`nome${i}`] = seguidores[i];
-                break
-            }
+        const segdo = seguindo[i];
+        if (seguidores.includes(segdo)) {
+            seguidorMutuo[`nome${i}`] = segdo;
+        } else {
+            naoSeguidor[`nome${i}`] = segdo;
         }
-        if (cont === 0) {
-        naoSeguidor[`nome${i}`] = seguidores[i];
-        }
-        cont = 0;
     }
+
     return naoSeguidor;
 }
 
