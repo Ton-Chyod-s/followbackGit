0fdd52e48dbe3bccfbc6e9704886935aed53db93
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-26 14:51:05 -0400

    doc: att

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
+server.get('/', (req, res) => {
+    res.json({
+        'Doc': "Welcome to the Git User Info API",
+        'Usage': "To get the user info, use the endpoint /:username where username is the github username",
+    });
+});
+
 server.get('/:username', async (req, res) => {
     const { username } = req.params;
     const result = await main(username);
