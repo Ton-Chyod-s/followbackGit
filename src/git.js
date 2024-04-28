301cbfef23fe5e6d713faa6b5a599b42864ea409
Klay <hix_x@hotmail.com>
2024-04-28 06:57:34 -0400

    doc: att

--- src/git.js
+++ src/git.js
@@ -1,17 +1,30 @@
+const { token } = require('./chave/token');
+
+const perPage = 500; // Número de resultados por página
+let page = 1; // Inicializa a página para o primeiro loop
+
 async function main(username) {
     const seguidores = [];
-    const followersResponse = await fetch(`https://api.github.com/users/${username}/followers`);
+    const followersResponse = await fetch(`https://api.github.com/users/${username}/followers`, {
+        headers: {
+            'Authorization': `token ${token}`
+        }
+    });
     const dataFollowers = await followersResponse.json();
     for (let i = 0; i < dataFollowers.length; i++) {
         seguidores.push(dataFollowers[i].login);
     }
 
     const seguindo = [];
-    const followingResponse = await fetch(`https://api.github.com/users/${username}/following`);
+    const followingResponse = await fetch(`https://api.github.com/users/${username}/following`, {
+        headers: {
+            'Authorization': `token ${token}`
+        }
+    });
     const dataFollowing = await followingResponse.json();
     for (let i = 0; i < dataFollowing.length; i++) {
         seguindo.push(dataFollowing[i].login);
-    }
+    };
 
     const seguidorMutuo = {};
     const naoSeguidor = {};
@@ -34,7 +47,7 @@ module.exports = { main };
 
 if (require.main === module) {
     (async () => {
-        const result = await main("Emakiflom");
+        const result = await main("silvniv");
         console.log(result);
     })();
 }
