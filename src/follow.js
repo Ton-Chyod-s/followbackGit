e9bf73f4a99f55d16d8ccb19dbc44e6506a1b5af
Klay <hix_x@hotmail.com>
2024-04-27 23:46:14 -0400

    doc: att

--- src/follow.js
+++ src/follow.js
@@ -3,7 +3,7 @@ const { token } = require('./chave/token');
 const perPage = 500; // Número de resultados por página
 let page = 1; // Inicializa a página para o primeiro loop
 
-async function follow(username) {
+async function follow(username, user) {
     while (true) {
         const seguidores = [];
         const followersResponse = await fetch(`https://api.github.com/users/${username}/followers?page=${page}&per_page=${perPage}`,{
@@ -29,7 +29,17 @@ async function follow(username) {
                 }
             });
             if (!followResponse.ok) {
-                console.error(`Erro ao seguir ${seguidores[i]}`);
+                // console.error(`Erro ao seguir ${seguidores[i]}`);
+                const seguindo = [];
+                const followingResponse = await fetch(`https://api.github.com/users/${user}/following`,{
+                    headers: {
+                        'Authorization': `token ${token}`
+                    }
+                });
+                const dataFollowing = await followingResponse.json();
+                for (let i = 0; i < dataFollowing.length; i++) {
+                    seguindo.push(dataFollowing[i].login);
+                }
             } 
         }
         page++;
@@ -41,7 +51,7 @@ module.exports = { follow };
 // Código de exemplo para testar a função follow
 if (require.main === module) {
     (async () => {
-        const result = await follow("programadorLhama");
+        const result = await follow("programadorLhama", "silvniv");
         console.log(result);
     })();
 }
