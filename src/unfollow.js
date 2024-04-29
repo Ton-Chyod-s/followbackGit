d2a991a282d225d17e48b9032cee30b4712ac42d
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-29 17:29:38 -0400

    doc: att

--- src/unfollow.js
+++ src/unfollow.js
@@ -1,5 +1,5 @@
 const { token } = require('./chave/token');
-const { funcSeguidores } = require('./utilitario/seguidores');
+const { funcSeguidores } = require('./utilitario/followers');
 
 const perPage = 500; // Número de resultados por página
 const page = 1; // Inicializa a página para o primeiro loop
