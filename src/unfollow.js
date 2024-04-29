8d4cfa48918774b495933257c9bd911f0f24d9bc
Klay <hix_x@hotmail.com>
2024-04-29 19:23:01 -0400

    doc: att

--- src/unfollow.js
+++ src/unfollow.js
@@ -1,11 +1,18 @@
-const { token } = require('./chave/token');
-const { funcSeguidores } = require('./utilitario/followers');
-
-const perPage = 500; // Número de resultados por página
-const page = 1; // Inicializa a página para o primeiro loop
+const { funcSeguidores } = require('./utility/followers');
+const { funcSeguir } = require('./utility/follow');
 
 async function unfollow(username) {
-    const lol = await funcSeguidores(username);
+    const seguidores = await funcSeguidores(username);
+    const seguindo = await funcSeguir(username);
+
+    return "Tudo oks!"
 }
 
 unfollow("silvniv")
+
+if (require.main === module) {
+    (async () => {
+        const result = await unfollow("silvniv");
+        console.log(result);
+    })();
+}
