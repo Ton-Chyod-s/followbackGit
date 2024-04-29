8f51a261c175fa584c74f655ba34b6a96e7e3ac2
Klay <hix_x@hotmail.com>
2024-04-29 18:35:45 -0400

    doc: att

---
+++ src/utilitario/follow.js
@@ -0,0 +1,36 @@
+const { page, perPage } = require('../utilitario/qtdePG');
+const { token } = require('../chave/token');
+
+async function funcSeguir(username) {
+    const seguindo = [];
+    while (true) {   
+            const respostaSeguindo = await fetch(`https://api.github.com/users/${username}/following?page=${page}&per_page=${perPage}`, {
+        headers: {
+            'Authorization': `token ${token}`
+            }
+        });
+        if (!respostaSeguindo.ok) {
+            const erro = await respostaSeguindo.json();
+            throw new Error(`GitHub API Error: ${erro.message}`);
+        }
+        const dataFollowing = await respostaSeguindo.json();
+        for (let i = 0; i < dataFollowing.length; i++) {
+            seguindo.push(dataFollowing[i].login);
+        };
+        if (dataFollowing.length < perPage) {
+            break; 
+        }
+        page++;
+    }
+    return seguindo;
+}
+
+module.exports = { funcSeguir };
+
+// Código de exemplo para testar a função follow
+if (require.main === module) {
+    (async () => {
+        const result = await funcSeguir("Ton-Chyod-s");
+        console.log(result);
+    })();
+}
