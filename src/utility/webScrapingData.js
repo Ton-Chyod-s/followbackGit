ebe5cfc88156328407d07f22e74fcac9f3e0ccd0
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-30 17:10:00 -0400

    doc: att

--- src/utility/webScrapingData.js
+++ src/utility/webScrapingData.js
@@ -1,11 +1,11 @@
 const puppeteer = require('puppeteer');
 
-async function webScrapingData(url) {
+async function webScrapingData() {
     const browser = await puppeteer.launch({
         headless: false,
     });
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto("https://github.com/settings/tokens");
 
     const data = await page.evaluate(() => {
         const title = document.querySelector('span');
@@ -20,7 +20,7 @@ async function webScrapingData(url) {
 // Código de exemplo para testar a função follow
 if (require.main === module) {
     (async () => {
-        const result = await webScrapingData("https://github.com/settings/tokens");
+        const result = await webScrapingData();
         console.log(result);
     })();
 }
