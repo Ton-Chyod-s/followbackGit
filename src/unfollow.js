8f58ba6d3e895a25c272dcabaeccad637da53c9a
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-30 10:00:17 -0400

    doc: att

--- src/unfollow.js
+++ src/unfollow.js
@@ -1,12 +1,22 @@
 const { main } = require("./checkFollower");
+const axios = require('axios');
+const { token } = require('./key/token');
 
 async function unfollow(username) {
     const followers = await main(username);
     for (let i in followers) {
-        console.log(followers[i])
-        
+        // console.log(followers[i])
+        axios.delete(`https://api.github.com/user/following/${followers[i]}`, {
+            headers: {
+                'Authorization': `token ${token}`,
+            }
+        }).then(response => {
+            console.log(`Você deixou de seguir o usuário ${nomeDoUsuario}`);
+        })
+        .catch(error => {
+            console.error('Ocorreu um erro:', error.response.data);
+        });
 
-        
     }
     return followers
 }
