c7db33ed0f22d250bd6e40f15fd199e91bbfd58d
Klay <hix_x@hotmail.com>
2024-04-30 18:55:27 -0400

    doc: att

--- src/utility/webScrapingData.js
+++ src/utility/webScrapingData.js
@@ -1,18 +1,32 @@
 const puppeteer = require('puppeteer');
 
-async function webScrapingData() {
+async function webScrapingData(nome,senha) {
     const browser = await puppeteer.launch({
         headless: false,
     });
     const page = await browser.newPage();
     await page.goto("https://github.com/settings/tokens");
 
-    const data = await page.evaluate(() => {
-        const title = document.querySelector('span');
-          return {
-            title
-        }
-    });
+    // Preencher um campo
+    await page.type('[id="login_field"]', nome);
+    // Preencher um campo
+    await page.type('[id="password"]', senha);
+    // clicar
+    await page.locator('[value="Sign in"]').click();
+
+
+
+    // // esperando até encontrar o selector
+    // await page.waitForSelector('table[id="tbDiarios"]');
+    // // Aguardar um pouco
+    // await new Promise(resolve => setTimeout(resolve, 500));
+
+    // const data = await page.evaluate(() => {
+    //     const title = document.querySelector('span');
+    //       return {
+    //         title
+    //     }
+    // });
 
     await browser.close();
 }
@@ -20,7 +34,7 @@ async function webScrapingData() {
 // Código de exemplo para testar a função follow
 if (require.main === module) {
     (async () => {
-        const result = await webScrapingData();
+        const result = await webScrapingData('hix_x@hotmail.com','Teamomtos2!');
         console.log(result);
     })();
 }
