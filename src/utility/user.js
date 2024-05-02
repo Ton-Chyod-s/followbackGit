4ac78bbff681e6dba7f9b7a4c44d4d8af4f3b8f8
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-02 16:41:19 -0400

    doc: att

--- src/utility/user.js
+++ src/utility/user.js
@@ -1,16 +1,21 @@
-// create file token.js
-if (!fs.existsSync(path.join(__dirname, 'key'))) {
-    fs.mkdirSync(path.join(__dirname, 'key'));
-}
+const fs = require('node:fs');
+const path = require('path');
 
-// write usuario in file
-(function user (email, senha) {
-    const token = `email = ${email} ;\nsenha = ${senha};\n\nmodule.exports = { email, senha };`
-    const filePath = path.join(__dirname, 'key', 'usuario.js');
-    fs.writeFile(filePath, token, 'utf8', function (err) {
-        if (err) return console.log(err);
-    });
-})();
+async function user (email, senha) {
+    // create file token.js
+    if (!fs.existsSync(path.join(__dirname, 'key'))) {
+        fs.mkdirSync(path.join(__dirname, 'key'));
+    }
+
+    // write usuario in file
+    (function () {
+        const token = `email = ${email} ;\nsenha = ${senha};\n\nmodule.exports = { email, senha };`
+        const filePath = path.join(__dirname, 'key', 'usuario.js');
+        fs.writeFile(filePath, token, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+    })();
+}
 
 // Código de exemplo para testar a função follow
 if (require.main === module) {
